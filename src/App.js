f22787469462312ef451ee2f83069c8fbb176a1b
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-07-20 09:42:00 +0600

    correction

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import * as firebase from 'firebase/app';
-import  {GoogleAuthProvider} from 'firebase/auth';
 import firebaseConfig from './firebase.config';
+//import 'firebase/auth';
+import {  getAuth, GoogleAuthProvider,  signInWithPopup } from "firebase/auth";
+import app from './firebase.config';
+import { useState } from 'react';
+const auth = getAuth(app);
 firebase.initializeApp(firebaseConfig)
 function App() {
-  const provider = new GoogleAuthProvider();
+  const [user, setUser] = useState({})
+  //const provider = new firebase.auth.GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  //const provider = new firebase.auth.GoogleAuthProvider();
+  const handleSignIn=()=>{
+    
+    signInWithPopup(auth,googleProvider)
+    .then(result => {
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
+  }
+
   return (
     <div className="App">
-      <button>sign-in</button>
+      <button onClick={handleSignIn}>sign-in</button>
     </div>
   );
 }
-
 export default App;
